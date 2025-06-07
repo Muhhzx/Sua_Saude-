a08b63783b10f27806d13b47f759b7ee22ba85cd
Murilo <murilogomes.dev@gmail.com>
2025-06-07 13:05:30 -0300

    atualizando os requirements

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from extensions import db
 from controllers.agua_controller import agua_bp
 from controllers.plano_controller import plano_bp
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///planner.db'
@@ -15,4 +16,4 @@ app.register_blueprint(plano_bp)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
